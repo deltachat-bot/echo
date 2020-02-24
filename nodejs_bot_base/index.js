b390ccc2a4eb4c45ab25f642feaae5e0869aca24
Simon Laux <mobile.info@simonlaux.de>
2020-02-24 23:56:53 +0100

    make it work and link it from main readme.

--- nodejs_bot_base/index.js
+++ nodejs_bot_base/index.js
@@ -3,7 +3,7 @@ const { deltachat, log } = require('deltachat-node-bot-base')
 // Start the deltachat core engine and handle incoming messages.
 deltachat.start((chat, message) => {
   log("Got a message: ", message.getText())
-  if (deltachat.getChatContacts(chat.getid()).length === 1) {
+  if (deltachat.getChatContacts(chat.getId()).length === 1) {
     // This is a 1-on-1 (aka "single") chat.
     deltachat.sendMessage(chat.getId(), message.getText())
   }
