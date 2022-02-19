790a4b6fc5e1efd5de1b2dbfbdbcbf75028dcca1
Simon Laux <mobile.info@simonlaux.de>
2022-02-19 23:15:32 +0100

    update rust bot to core 1.75

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,4 @@
+use async_std::channel;
 use futures_lite::future::FutureExt;
 use std::env::{current_dir, vars};
 use std::sync::Arc;
@@ -6,14 +7,21 @@ use deltachat::chat::*;
 use deltachat::config;
 use deltachat::constants::{Chattype, Viewtype, DC_CONTACT_ID_SELF};
 use deltachat::context::*;
-use deltachat::error::Error;
 use deltachat::message::*;
 use deltachat::EventType;
 
-async fn handle_message(_ctx: &Context, chat_id: ChatId, msg_id: MsgId) -> Result<(), Error> {
-    let chat = Chat::load_from_db(_ctx, chat_id).await?;
-    let msg = Message::load_from_db(_ctx, msg_id).await?;
+async fn handle_message(
+    ctx: &Context,
+    chat_id: ChatId,
+    msg_id: MsgId,
+) -> Result<(), anyhow::Error> {
+    let mut chat = Chat::load_from_db(ctx, chat_id).await?;
+    if chat.is_contact_request() {
+        chat_id.accept(ctx).await?;
+        chat = Chat::load_from_db(ctx, chat_id).await?;
+    }
 
+    let msg = Message::load_from_db(ctx, msg_id).await?;
     if msg.get_from_id() == DC_CONTACT_ID_SELF {
         // prevent loop (don't react to own messages)
         return Ok(());
@@ -21,34 +29,34 @@ async fn handle_message(_ctx: &Context, chat_id: ChatId, msg_id: MsgId) -> Resul
 
     println!(
         "recieved message '{}' in chat with type {:?}",
-        msg.get_text().unwrap_or("".to_owned()),
+        msg.get_text().unwrap_or_else(|| "".to_owned()),
         chat.get_type()
     );
 
     if chat.get_type() == Chattype::Single {
         let mut message = Message::new(Viewtype::Text);
         message.set_text(msg.get_text());
-        send_msg(_ctx, chat_id, &mut message).await?;
+        send_msg(ctx, chat_id, &mut message).await?;
     }
 
     Ok(())
 }
 
-async fn cb(_ctx: &Context, event: EventType) {
+async fn cb(ctx: &Context, event: EventType) {
     //println!("[{:?}]", event);
 
     match event {
         EventType::ConfigureProgress { progress, comment } => {
             println!("  progress: {} {:?}", progress, comment);
         }
-        EventType::Info(msg)
-        | EventType::Warning(msg)
-        | EventType::Error(msg)
-        | EventType::ErrorNetwork(msg) => {
+        EventType::Info(msg) | EventType::Warning(msg) | EventType::Error(msg) => {
             println!(" {}", msg);
         }
-        EventType::MsgsChanged { chat_id, msg_id } => {
-            match handle_message(_ctx, chat_id, msg_id).await {
+        EventType::ConnectivityChanged => {
+            println!("ConnectivityChanged: {:?}", ctx.get_connectivity().await);
+        }
+        EventType::IncomingMsg { chat_id, msg_id } => {
+            match handle_message(ctx, chat_id, msg_id).await {
                 Err(err) => {
                     print!("{}", err);
                 }
@@ -62,12 +70,12 @@ async fn cb(_ctx: &Context, event: EventType) {
 }
 
 #[async_std::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let dbdir = current_dir().unwrap().join("deltachat-db");
     std::fs::create_dir_all(dbdir.clone()).unwrap();
     let dbfile = dbdir.join("db.sqlite");
     println!("creating database {:?}", dbfile);
-    let ctx = Context::new("FakeOs".into(), dbfile.into(), 0)
+    let ctx = Context::new(dbfile.into(), 0)
         .await
         .expect("Failed to create context");
 
@@ -77,11 +85,11 @@ async fn main() {
 
     let events = ctx.get_event_emitter();
 
-    let (interrupt_send, interrupt_recv) = async_std::sync::channel(1);
-    ctrlc::set_handler(move || async_std::task::block_on(interrupt_send.send(())))
+    let (interrupt_send, interrupt_recv) = channel::bounded(1);
+    ctrlc::set_handler(move || async_std::task::block_on(interrupt_send.send(())).unwrap())
         .expect("Error setting Ctrl-C handler");
 
-    let is_configured = ctx.get_config_bool(config::Config::Configured).await;
+    let is_configured = ctx.get_config_bool(config::Config::Configured).await?;
     if !is_configured {
         println!("configuring");
         if let Some(addr) = vars().find(|key| key.0 == "addr") {
@@ -133,4 +141,6 @@ async fn main() {
     while let Some(event) = events.recv().await {
         println!("ignoring event {:?}", event);
     }
+
+    Ok(())
 }
