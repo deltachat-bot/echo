ab99508dde6d8678259edb9b22cd67541b52bcbf
link2xt <link2xt@testrun.org>
2025-05-26 18:03:55 +0000

    Configure account and print event types

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -26,3 +26,26 @@ foreach(var item in res)
 {
   Console.WriteLine("{0}={1}", item.Key, item.Value);
 }
+
+bool isConfigured = await rpc.InvokeAsync<bool>("is_configured", accountId);
+if(!isConfigured) {
+  Console.WriteLine("not configured");
+  await rpc.InvokeAsync("set_config_from_qr", accountId, "dcaccount:https://nine.testrun.org/new");
+  await rpc.InvokeAsync("configure", accountId);
+}
+
+await rpc.InvokeAsync("start_io_for_all_accounts");
+
+while(true) {
+  Event res2 = await rpc.InvokeAsync<Event>("get_next_event");
+  Console.WriteLine("Got event {0} {1}", res2.contextId, res2.@event.kind);
+}
+
+public class Event {
+  public int contextId;
+  public EventType @event;
+}
+
+public class EventType {
+  public string kind;
+}
