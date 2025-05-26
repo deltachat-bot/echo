d1dfd6ece4f9d15d021014fe32dfe3d9a139cd98
link2xt <link2xt@testrun.org>
2025-05-26 17:35:24 +0000

    Create .NET bot

---
+++ dotnet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using StreamJsonRpc;
+
+Process childProcess = Process.Start(new ProcessStartInfo("deltachat-rpc-server")
+{
+    RedirectStandardInput = true,
+    RedirectStandardOutput = true,
+});
+
+JsonRpc rpc = new JsonRpc(new StreamJsonRpc.NewLineDelimitedMessageHandler(childProcess.StandardInput.BaseStream, childProcess.StandardOutput.BaseStream, new StreamJsonRpc.JsonMessageFormatter()));
+rpc.StartListening();
+
+var accounts = await rpc.InvokeAsync<int[]>("get_all_account_ids");
+int accountId;
+if(accounts.Length == 0) {
+  accountId = await rpc.InvokeAsync<int>("add_account");
+} else {
+  accountId = accounts[0];
+}
+
+var res = await rpc.InvokeAsync<Dictionary<String, String>>("get_system_info");
+
+foreach(var item in res)
+{
+  Console.WriteLine("{0}={1}", item.Key, item.Value);
+}
---
+++ dotnet/README.md
@@ -0,0 +1,10 @@
+To run C# bot,
+you need to install .NET.
+On Arch Linux it can be done with
+`pacman -S dotnet-sdk`.
+
+To build the program,
+run `dotnet build`.
+
+To run the program,
+run `dotnet run`.
